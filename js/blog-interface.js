fbf46bdb67f321c45f49f3f1b7b54cac35ea161d
Franz Knupfer <franzknupfer@hotmail.com>
2016-11-22 14:48:53 -0800

    integrated for build

--- js/blog-interface.js
+++ js/blog-interface.js
@@ -1,3 +1,10 @@
+var Entry = require('./../js/blog.js').entryModule;
+
+var clearFields = function() {
+  $("input#entry").val("");
+  $("textarea#content").val("");
+};
+
 $(document).ready(function() {
   $("form#new-blog-entry").submit(function(event) {
     event.preventDefault();
@@ -6,9 +13,9 @@ $(document).ready(function() {
     var content = $("textarea#content").val();
 
     var blogEntry = new Entry(entry, content);
-    
+
     $(".all-entries").append("<h3>" + blogEntry.entryName + "</h3>");
-    $(".all-entries").append("<h5>" + blogEntry.getTeaser() + "</h5>");
+    $(".all-entries").append("<h5>" + blogEntry.getTeaser() + " / " + blogEntry.countWords() + "</h5>");
     $(".all-entries").append("<p>" + blogEntry.entryContent + "<p>");
 
     clearFields();
--- js/blog.js
+++ js/blog.js
@@ -7,20 +7,19 @@ function Entry(entryName, entryContent) {
   this.entryContent = entryContent;
 }
 
+Entry.prototype.countWords = function() {
+  return this.entryContent.split(" ").length + " words";
+};
+
 Entry.prototype.getTeaser = function() {
-  var entry = this.entryContent.split(" ")
-  var teaser = []
+  var entry = this.entryContent.split(" ");
+  var teaser = [];
   for (var i = 0; i <= entry.length; i++) {
     teaser.push(entry[i]);
     if (i === 7) { break; }
   }
-  teaser = teaser.join(' ') + "..."
+  teaser = teaser.join(' ') + "...";
   return teaser;
-}
-
-var clearFields = function() {
-  $("input#entry").val("");
-  $("textarea#content").val("");
-}
+};
 
 exports.entryModule = Entry;
